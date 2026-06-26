2f599167942bb7aab1f6b2ad58f125a7a7640390
yaswanthlekkala <yaswanthlekkala.tech@gmail.com>
2026-06-26 21:46:23 +0530

    Add files via upload

---
+++ dutchnationalfalg.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int low = 0, mid = 0, high = n - 1;
+
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else {
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
